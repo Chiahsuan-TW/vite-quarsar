26cb268adc2bd4e6a6aeed3caa4f34c2b7c4911c
Chiahsuan Chou <belinda05021992@gmail.com>
2022-01-08 14:24:50 +0800

    practice environment setting complete

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
+    <div id="q-app" style="min-height: 100vh">
+      <div class="q-pa-md">
+        <q-table
+          title="Treats"
+          :rows="rows"
+          :columns="columns"
+          row-key="name"
+        ></q-table>
+      </div>
+    </div>
   </div>
 </template>
 
